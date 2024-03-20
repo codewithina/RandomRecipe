a8ab4ddc6aa0c21eb524e282fed552dd5899234d
Ina Burström <inaburstrom@hotmail.com>
2024-03-20 16:25:58 +0100

    Added recipe struct and recipe controller.

---
+++ RandomRecipe/Recipe.swift
@@ -0,0 +1,14 @@
+//
+//  Recipe.swift
+//  RandomRecipe
+//
+//  Created by Ina Burström on 2024-03-20.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var category: String
+    var ingredients : [String]
+}
---
+++ RandomRecipe/RecipeController.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeController.swift
+//  RandomRecipe
+//
+//  Created by Ina Burström on 2024-03-20.
+//
+
+import Foundation
+
+class RecipeController {
+    var recipes: [Recipe] = []
+    
+    func addRecipe(recipe: Recipe) {
+        recipes.append(recipe)
+    }
+    
+}
